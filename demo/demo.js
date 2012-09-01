a549fdbc3c8099a96c5ceb10ea4edf2ba9c40934
Oscar Finnsson <oscar.finnsson@gmail.com>
2012-09-02 00:29:48 +0200

    Updating demo in gh-pages

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,35 @@
 require(['jquery', 'knockout', 'underscore', 'pager', 'bootstrap'], function ($, ko, _, pager, bootstrap) {
     var viewModel = {
         name:ko.observable("Pelle"),
-        description: ko.observable('pl')
+        description:ko.observable('pl'),
+        afterFryIsDisplayed:function () {
+            $('body').css("background-color", "#FF9999");
+        },
+        beforeFryIsHidden:function () {
+            $('body').stop().css("background-color", "#FFFFFF");
+        },
+        showFry:function (page, callback) {
+            $(page.element).fadeIn(1000, callback);
+        },
+        hideFry:function (page, callback) {
+            $(page.element).fadeOut(1000, function () {
+                $(page.element).hide();
+                if (callback) {
+                    callback();
+                }
+            });
+        },
+        textLoader: function(page, element) {
+            var loader = {};
+            var txt = $('<div></div>', {text: 'Loading ' + page.getValue().title});
+            loader.load = function() {
+                $(element).append(txt);
+            };
+            loader.unload = function() {
+                txt.remove();
+            };
+            return loader;
+        }
     };
 
     window.requireVM = function (module) {
